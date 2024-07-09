3755c4607de4e7a151f2cdfa440687b9d4d8223e
Wilfison <wilfisonbatista@gmail.com>
2024-07-09 16:00:11 -0300

    feat: Update popup window size in background.js

--- public/background.js
+++ public/background.js
@@ -1,8 +1,9 @@
+/* eslint-disable no-undef */
 chrome.action.onClicked.addListener(() => {
   chrome.windows.create({
     url: chrome.runtime.getURL("popup.html"),
     type: "popup",
-    width: 600,
-    height: 750,
+    width: 650,
+    height: 800,
   });
 });
